6600dee7016053a374cd68785bfe53c466b40a8a
manthranatarajan <107138263+manthranatarajan@users.noreply.github.com>
2025-01-17 11:46:55 -0600

    testing queries format

---
+++ queriestest.pl
@@ -0,0 +1,9 @@
+%% Query Examples (for Testing)
+?- harassment_response("message").
+?- threat_response("message").
+?- exclusion_response("message").
+?- discrimination_response("message").
+?- unclassified_response("message").
+?- at_risk_message("message").
+?- maybe_at_risk("message").
+?- not_at_risk("message").
